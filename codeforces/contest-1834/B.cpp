d5eef2df865f49fdb9d4dcd2e21185f4f945ae7c
lavinraj <lavinrajmohan@gmail.com>
2023-06-18 18:00:34 +0530

    finished

---
+++ codeforces/contest-1834/B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+#define LOOP(i, low, high, shift) for (int i = low; (low < high > 0 ? i < high : i >= high); (low < high ? i += shift : i -= shift))
+#define LOOP1(high) LOOP(i, 0, high, 1)
+#define LOOP2(i, high) LOOP(i, 0, high, 1)
+#define LOOP3(i, low, high) LOOP(i, low, high, 1)
+#define LOOP4(i, low, high, shift) LOOP(i, low, high, shift)
+#define FIFTH_ARG(a, b, c, d, e, ...) e
+#define GET_LOOP(...) FIFTH_ARG(__VA_ARGS__, LOOP4, LOOP3, LOOP2, LOOP1)
+#define For(...) GET_LOOP(__VA_ARGS__)(__VA_ARGS__)
+
+using namespace std;
+
+void print(){cout << "\n";}template<typename T, typename...types> void print(const T& first, const types&...args) {cout << first << " "; print(args...);}
+
+void solve(){
+  string a, b;
+  cin >> a >> b;
+  
+  if (sz(a) < sz(b)) {
+    int ans = b[0] - '0';
+    print(ans + 9 * (sz(b) - 1));
+  }
+  else {
+    int ans = b[0] - a[0];
+    for (int i = 1; i < sz(a); i++) {
+      if (a[i - 1] ^ b[i - 1]) {
+        ans += 9;
+        a[i] = '9';
+        b[i] = '0';
+      }
+      else ans += abs(b[i] - a[i]);
+    }
+
+    print(ans);
+  }
+}
+
+int main() { 
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    //cout << "Case #" << i << ": ";
+        solve();
+  }
+  return 0;
+}
