61684cd563127812f9fa893a915fddd12172121c
lavinraj <lavinrajmohan@gmail.com>
2023-07-05 21:55:35 +0530

    finished

---
+++ codechef/starter-97/DICEGAME2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+
+#define LOOP(i, low, high, shift) for (int i = low; (low < high > 0 ? i < high : i >= high); (low < high ? i += shift : i -= shift))
+#define LOOP1(high) LOOP(i, 0, high, 1)
+#define LOOP2(i, high) LOOP(i, 0, high, 1)
+#define LOOP3(i, low, high) LOOP(i, low, high, 1)
+#define LOOP4(i, low, high, shift) LOOP(i, low, high, shift)
+#define FIFTH_ARG(a, b, c, d, e, ...) e
+#define GET_LOOP(...) FIFTH_ARG(__VA_ARGS__, LOOP4, LOOP3, LOOP2, LOOP1)
+#define For(...) GET_LOOP(__VA_ARGS__)(__VA_ARGS__)
+
+using namespace std;
+
+void print(){cout << "\n";}template<typename T, typename...types> void print(const T& first, const types&...args) {cout << first << " "; print(args...);}
+
+void solve(){
+  vector<int> a(3), b(3);
+  For (3) cin >> a[i];
+  For (3) cin >> b[i];
+
+
+  sort(all(a), greater<int>());
+  sort(all(b), greater<int>());
+
+  print(a[0] + a[1] == b[0] + b[1] ? "TIE" : a [0] + a[1] < b[0] + b[1] ? "BOB" : "ALICE");
+}
+
+int main() { 
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    //cout << "Case #" << i << ": ";
+        solve();
+  }
+  return 0;
+}
