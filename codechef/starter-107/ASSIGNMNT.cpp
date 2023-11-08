4478b75c4f01d548c7bd1f7d7489d293f2b369d3
lavinraj <lavinrajmohan@gmail.com>
2023-11-08 21:58:34 +0530

    codechef starter 107 finished

---
+++ codechef/starter-107/ASSIGNMNT.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#ifdef DEBUG
+#include "/home/lavinraj/career/code-library/debug.cpp"
+#else
+#define dbg(x...) 32
+#endif
+#define len(x) int((x).size())
+
+using namespace std;
+
+void print(){cout << "\n";}template<typename T, typename...types> void print(const T& first, const types&...args) {cout << first << " "; print(args...);}
+
+void solve() {
+  int x, y, z;
+  cin >> x >> y >> z;
+
+  int total = x * y;
+  int time = z * 24 * 60;
+
+  print(total <= time ? "YES" : "NO");
+}
+
+int main() { 
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
