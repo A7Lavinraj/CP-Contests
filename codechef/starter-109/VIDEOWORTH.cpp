a5f61349a11ac4bc72d5dda37fb8450e3d441d81
lavinraj <lavinrajmohan@gmail.com>
2023-11-22 22:07:15 +0530

    starter 109

---
+++ codechef/starter-109/VIDEOWORTH.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#ifdef DEBUG
+#include "/home/lavinraj/career/cp/code-library/debug.cpp"
+#else
+#define dbg(x...)
+#endif
+#define len(x) int((x).size())
+
+using namespace std;
+
+void solve() {
+  int N;
+  cin >> N;
+
+  cout << (N * 24 * 1000) << '\n';
+}
+
+int main() { 
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
