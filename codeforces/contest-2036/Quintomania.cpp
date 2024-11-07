912dae83a62776c9151d16137797b4c73b7685f6
A7Lavinraj <lavinrajmohan@gmail.com>
2024-11-07 11:33:13 +0530

    chore: codeforces round 984 (div 3)

---
+++ codeforces/contest-2036/Quintomania.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream &os, const pair<T1, T2> &p) {
+  os << "(" << p.first << ", " << p.second << ")";
+  return os;
+}
+
+template<typename T>
+ostream& operator<<(ostream &os, const vector<T> &v) {
+  os << "[";
+  for (int i = 0; i < v.size(); i++) {
+    if (i > 0) os << ", ";
+    os << v[i];
+  }
+  os << "]";
+  return os;
+}
+
+template<typename T>
+ostream& operator<<(ostream &os, const set<T> &s) {
+  os << "{";
+  for (auto it = s.begin(); it != s.end(); ++it) {
+    if (it != s.begin()) os << ", ";
+    os << *it;
+  }
+  os << "}";
+  return os;
+}
+
+template<typename K, typename V>
+ostream& operator<<(ostream &os, const map<K, V> &m) {
+  os << "{";
+  for (auto it = m.begin(); it != m.end(); ++it) {
+    if (it != m.begin()) os << ", ";
+    os << it->first << ": " << it->second;
+  }
+  os << "}";
+  return os;
+}
+
+template<typename T>
+void debug_out(T val) {
+  cerr << val;
+}
+
+template<typename T, typename... Args>
+void debug_out(T val, Args... args) {
+  cerr << val << " ";
+  debug_out(args...);
+}
+
+#define dbg(...) do { cerr << "[" << #__VA_ARGS__ << "]: "; debug_out(__VA_ARGS__); cerr << endl; } while (0)
+
+void testcase() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (int i = 0; i < n - 1; i++) {
+    if (abs(a[i] - a[i + 1]) != 5 && abs(a[i] - a[i + 1]) != 7) {
+      cout << "NO" << '\n';
+      return;
+    }
+  }
+
+  cout << "YES" << '\n';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    // cout << "Case #" << i << ": ";
+    testcase();
+  }
+  return 0;
+}
